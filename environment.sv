bc776ec2b664fd68d667d46e051c53bf4b18013d
Leon <auzyze@gmail.com>
2013-07-17 17:06:45 +0800

    Update environment.sv

--- environment.sv
+++ environment.sv
@@ -1,20 +1,21 @@
 class Environment;
 
-  OP_generator  gen[];
-  mailbox       gen2drv[];
-  event         drv2gen[];
-  Driver        drv[];
-  Monitor       mon[];
+  OP_generator  gen;
+  mailbox       gen2drv;
+  event         drv2gen;
+  Driver        drv;
+  Monitor       mon;
   Config        cfg;
   Scoreboard    scb;
   Coverage      cov;
   
-  virtual fifo_if.TB test_if[];
-  virtual cpu_if.Cfg mif;
+  CPU_driver    cpu;
+
+  vFifo_TB      op_intf;      //virtual interface
+  vCPU_T        cfg_intf;     //virtual interface
   
-  CPU_driver cpu;
-  
-  extern function new(test_if);
+  extern function new(input vFifo_TB op_intf,
+                      input vCPU_T cfg_intf);
   
   extern virtual function void gen_cfg();
   extern virtual function void build();
@@ -25,8 +26,10 @@ endclass: Environment
 
 ///////////////////////////////////////////////////
 //construct an environment instance
-function Environment::new(input test_if);
-  this.test_if = test_if;
+function Environment::new(input vFifo_TB op_intf,
+                          input vCPU_T cfg_intf);
+  this.op_intf = op_intf;
+  this.cfg_intf = cfg_intf;
   
   if ($test$plusargs("ntb_random_seed")) begin
     int seed;
@@ -49,15 +52,16 @@ endfunction : gen_cfg
 //////////////////////////////////////////////////
 //Build the environment objects
 function void Environment::build();
-  cpu = new(mif,cfg);
+  cpu = new(cfg_intf,cfg);
   gen = new();
   drv = new();
-  gen2drv = new;
-  drv2gen = new;
+  gen2drv = new();
+  drv2gen = new();
   scb = new(cfg);
   cov = new();
   mon = new();  
   
+  //??????
   //Connect scoreboard to drvier & monitor with callbacks
   begin
     Scb_Driver_cbs sdc = new(scb);
@@ -68,15 +72,15 @@ function void Environment::build();
   
   //connect coverage to monitor with callbacks
   begin
-    Cov_Monitor_cbs smc = new(cov);
-    mon.cbsq.push_back(smc);
+    Cov_Monitor_cbs cmc = new(cov);
+    mon.cbsq.push_back(cmc);
   end
 
 endfunction : build
 
 
 //////////////////////////////////////////////////////
-//Start the transactions: generator, driver, monior
+//Start the transactors: generator, driver, monior
 task Environment::run();
   
   //The CPU interface initializes before anyone else
@@ -89,7 +93,7 @@ task Environment::run();
   //Monitor
   mon.run();
   
-  repeat (10000)@(test_if.cb);
+  repeat (10000)@(op_intf.cb);
   
 endtask : run
   
@@ -105,12 +109,40 @@ endfunction : wrap_up
 
 
 
+////////////////////////////////////////////////////////
+//fifo operation: write, read or both. suport single or burst
+//randomization: operation number, operation length, operation delay ...
+//
+class fifo_OP extends BaseOP;
+  rand bit [15:0] wr_num;
+  rand bit [15:0] wr_len;
+  rand bit [31:0] rd_data [];
+  rand bit        wr_dly [];
+  
+  rand bit [15:0] rd_num;
+  rand bit [15:0] rd_len;
+  rand bit        rd_dly [];
+  
+  
+  extern function new();
+  extern function void post_randomize();
+  extern vitrual function bit compare(input BaseOP to);
+  extern virtual function void display(input string prefix="");
+  extern virtual function void copy_data(input fifo_OP copy);
+  extern virtual function BaseOP copy(input BaseOP to=null);
+  
+...
+
+endclass
+
+
+
 ///////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////
 //FIFO OPeration Generator
 class OP_generator;
-  fifo_OP blueprint; //Blueprint for generator
+  fifo_OP blueprint; //Blueprint for generator, can be replaced with objects derived from fifo_OP
   mailbox gen2drv;
   event   drv2gen;
   
@@ -138,12 +170,6 @@ class OP_generator;
 endclass : OP_generator
 
 
-class fifo_OP extends BaseOP;
-...
-
-endclass
-
-
 
 //////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////
@@ -151,11 +177,13 @@ endclass
 class Driver;
   mailbox gen2drv;
   event   drv2gen;
-  test_if op_intf;    //virtual interface for operation transimitting
+  vFifo_TB op_intf;     //virtual interface for operation transimitting
+  
+  Driver_cbs cbsq[$];     //Queue of callback objects
   
   extern function new(input mailbox gen2drv,
                       input event drv2gen,
-                      input test_if op_intf);
+                      input vFifo_TB op_intf);
   extern task run();
   extern task send (input fifo_OP operation);
   
@@ -164,7 +192,7 @@ endclass : Driver
 //
 function Driver::new(input mailbox gen2drv,
                      input event drv2gen,
-                     input test_if op_intf);
+                     input vFifo_TB op_intf);
   this.gen2drv = gen2drv;
   this.drv2gen = drv2gen;
   this.op_intf = op_intf;
@@ -173,7 +201,6 @@ endfunction : new
 //run() : run the driver
 task Driver::run();
   fifo_OP operation;
-  bit drop = 0;
   
   //Initialize ports
   op_intf.cb.wr_en <= 0;
@@ -181,29 +208,69 @@ task Driver::run();
   op_intf.cb.rd_en <= 0;
   
   forever begin
-    gen2drv.put(operation);
+    gen2drv.peek(operation);      //"peek" task gets a copy of data in mailbox but doesn't remove it
     send(operation);
-    gen2drv.get(operation);
+    gen2drv.get(operation);       //remove the data with "get" task after operation has been sent
     ->drv2gen;
   end
 endtask : run
 
 //send() : Send a operation into DUT
 task Driver::send(input fifo_OP operation);
-  for (int i=0; i<=op_len; i++) begin
-    if (op_type == WRITE) begin
-      ...
-    end
-    else if (op_type == READ) begin
-      ...
-    end
-    @(op_intf.cb);
+  
+  fork begin
+    //thread of writing
+    //thread of reading
   end
-  op_intf.cb.wr_en <= `z;
-  op_intf.cb.rd_en <= `z;
-  op_intf.cb.wr_data <= 32'bx;
-
+  
 endtask
+
+
+
+
+/////////////////////////////////////////////////////////////
+//Monitor
+
+class Monitor;
+  vFifo_TB op_intf;
+  
+  extern function new(input vFifo_TB op_intf);
+  extern task run();
+  extern task receive(output RData output_d);    //what should be received?
+endclass : Monitor
+
+//new
+function Monitor::new(input vFifo_TB op_intf);
+  this.op_intf = op_intf;
+endfunction
+
+
+task Monitor::run();
+  RData output_d;
+  
+  forever begin
+    receive(output_d);
+  end
+  
+endtask : run
+
+
+//receive task
+task Monitor::receive(output RData output_d)
+
+.....
+
+endtask : receive
+
+
+
+///////////////////////////////////////////////////
+//the scoreboard class
+
+
+
+
+
 
 
 
