fdce2b239bd780a8d210b4919369b304275aedce
Leon <auzyze@gmail.com>
2013-07-18 12:04:50 +0800

    Update cpu_if.sv

--- cpu_if.sv
+++ cpu_if.sv
@@ -5,11 +5,7 @@ logic Rd_Wr;                    //1-read, 0-write
 logic [3:0] Addr;               //accessing address
 logic [7:0] DataIn, DataOut;    //data
 
-
-modport Peripheral
-        (input CS, Rd_Wr, Addr, Datain,
-         output DataOut);
-
+//"local bus" : asynchoronous intel mode, no clocking needed
 modport CFG
         (output CS, Rd_Wr, Addr, DataIn,
          input  DataOut);
