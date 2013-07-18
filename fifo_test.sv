a50b3bf1bd50cbcc650df9d447b830546a3d3462
Leon <auzyze@gmail.com>
2013-07-18 20:01:04 +0800

    Update fifo_test.sv

--- fifo_test.sv
+++ fifo_test.sv
@@ -9,6 +9,13 @@ initial begin
   env = new(op_intf,cfg_intf);
   env.gen_cfg();
   env.build();
+  
+  /*A method to use new transaction extended from base "fifo_op" */
+  //begin
+  //  fifo_op_ext new_op = new();
+  //  env.gen.blueprint = new_op;
+  //end
+  
   env.run();
   env.wrap_up();
 end
